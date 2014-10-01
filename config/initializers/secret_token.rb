96cd4f64418c2b81050a4cf92d2b9dbe5d4aae79
George Thomas <georgetamayothomas@gmail.com>
2014-10-01 16:42:33 +0100

    New Rails Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MayBall::Application.config.secret_key_base = 'cca95380dc9acf32fcad3a40a6d3697ae023ed0d2892365f3e95783098360e130932927d1d6baec09d4cc94cc2dd52128e7063f6266a9a265fd15cdfefab76f9'
