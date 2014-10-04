168e8159f2439daf72270c2eeceefd55530a1680
George Thomas <georgetamayothomas@gmail.com>
2014-10-04 23:27:48 +0100

    User.from_omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:raven]
+
+  def self.from_omniauth(auth_hash)
+    data = auth_hash.info
+    user = User.where(:email => data["email"]).first
+
+    unless user
+        user = User.create(name: data["name"], email: data["email"],
+            password: Devise.friendly_token[0,20])
+    end
+    user
+  end
 end
