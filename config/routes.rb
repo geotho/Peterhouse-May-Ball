21edede2550e208d152289fab0f255588b583401
George Thomas <georgetamayothomas@gmail.com>
2014-10-14 18:25:26 +0100

    Set the user home as root

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ MayBall::Application.routes.draw do
   # You can have the root of your site routed with "root"
   devise_scope :user do
     authenticated :user do
-      root 'tickets#index', as: :authenticated_root
+      root 'users#home', as: :authenticated_root
     end
 
     unauthenticated do
