e3f6fe7c830a403a3bcdf175c4723513599bb2ad
George Thomas <georgetamayothomas@gmail.com>
2015-01-01 18:05:07 +0000

    TicketType after_initialize to set default values

--- app/models/ticket_type.rb
+++ app/models/ticket_type.rb
@@ -1,3 +1,10 @@
 class TicketType < ActiveRecord::Base
+  after_initialize :assign_default_values
   validates :name, :price, :max_number, :number_allocated, :for_sale, :size, presence: true
+  private
+    def assign_default_values
+      self.for_sale = false if self.for_sale.nil?
+      self.number_allocated = 0 if self.number_allocated.nil?
+      self.size = 2 if self.size.nil?
+    end
 end
