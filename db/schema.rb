b2aae62756f92fa5b40b65d4b5ddea0233bbd259
George Thomas <georgetamayothomas@gmail.com>
2015-01-14 21:15:07 +0000

    Run migration to add ticket to charges

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150114163916) do
+ActiveRecord::Schema.define(version: 20150114202115) do
 
   create_table "charges", force: true do |t|
     t.integer  "user_id"
@@ -19,8 +19,10 @@ ActiveRecord::Schema.define(version: 20150114163916) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "ticket_id"
   end
 
+  add_index "charges", ["ticket_id"], name: "index_charges_on_ticket_id"
   add_index "charges", ["user_id"], name: "index_charges_on_user_id"
 
   create_table "payments", force: true do |t|
