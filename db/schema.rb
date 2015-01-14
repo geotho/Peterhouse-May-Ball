922413bf2956774aeade0048fc7424b331a35201
George Thomas <georgetamayothomas@gmail.com>
2015-01-14 16:11:24 +0000

    Migrate db to create Payment table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150109202657) do
+ActiveRecord::Schema.define(version: 20150114160457) do
 
   create_table "charges", force: true do |t|
     t.integer  "user_id"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 20150109202657) do
   end
 
   add_index "charges", ["user_id"], name: "index_charges_on_user_id"
+
+  create_table "payments", force: true do |t|
+    t.integer  "user_id"
+    t.decimal  "amount",     precision: 6, scale: 2
+    t.integer  "method"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "payments", ["user_id"], name: "index_payments_on_user_id"
 
   create_table "ticket_types", force: true do |t|
     t.string   "name"
