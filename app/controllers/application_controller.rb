b38c69b119c2321b983eaca8ef5cc671e691cec0
George Thomas <georgetamayothomas@gmail.com>
2015-02-08 02:17:07 +0000

    Just add, not replace, params for signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
 
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :surname, :matric) }
+      devise_parameter_sanitizer.for(:sign_up) << :first_name << :surname << :matric
     end
 end
